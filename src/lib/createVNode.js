fa35b5570a2657b02223ea85dfe69277586d2d57
현지수 <hyunzsu@naver.com>
2025-07-17 00:17:30 +0900

    feat: createVNode 함수 구현

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -1,3 +1,7 @@
 export function createVNode(type, props, ...children) {
-  return {};
+  return {
+    type,
+    props: props || null,
+    children: children.flat(Infinity).filter((child) => child === 0 || Boolean(child)),
+  };
 }
